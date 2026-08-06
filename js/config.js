992be16350a7d111302cdadfeeb8b095da356c48
Trần Quốc Khánh <meongutw@gmail.com>
2026-08-06 08:20:59 +0700

    chuyển lại api

--- js/config.js
+++ js/config.js
@@ -4,8 +4,8 @@
 
 // URL cho môi trường Local Development (dùng với XAMPP, Laragon, etc.)
 // const API_BASE_URL = 'http://magic-hop-api.test/api';
-// let API_BASE_URL = 'https://magic-hop-api-1.onrender.com/api';
-let API_BASE_URL = 'http://magic-hop-api.test/api';
+let API_BASE_URL = 'https://magic-hop-api-1.onrender.com/api';
+// let API_BASE_URL = 'http://magic-hop-api.test/api';
 
 // URL cho môi trường Production (Vercel)
 // const API_BASE_URL = 'https://magic-hop-ku07tktq4-meongutw-5405s-projects.vercel.app/api';
